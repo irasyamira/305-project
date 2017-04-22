10d7825dcc6b1e60f1902efbc10ac7d20c612c7d
irasyamira <isuk218@UoA.auckland.ac.nz>
2017-04-22 19:59:43 +1200

    added more code to tank_test

--- mini_project_isuk218/simulation/modelsim/mini_project_isuk218_6_1200mv_85c_slow.vho
+++ mini_project_isuk218/simulation/modelsim/mini_project_isuk218_6_1200mv_85c_slow.vho
@@ -16,7 +16,7 @@
 -- PROGRAM "Quartus II 64-Bit"
 -- VERSION "Version 13.0.0 Build 156 04/24/2013 SJ Full Version"
 
--- DATE "04/22/2017 18:21:19"
+-- DATE "04/22/2017 19:52:26"
 
 -- 
 -- Device: Altera EP3C16F484C6 Package FBGA484
--- mini_project_isuk218/tank_test.vhd
+++ mini_project_isuk218/tank_test.vhd
@@ -12,17 +12,12 @@ end tank_test;
 
 architecture bhv of tank_test is
 
-	signal red_data, green_data, blue_data, vert_sync_int: std_logic;
-	signal tank_size: std_logic_vector(9 downto 0);
-	signal tank_Y_pos: std_logic_vector(9 downto 0); 
-	signal tank_X_pos: std_logic_vector(9 downto 0);
-	signal target_size: std_logic_vector(9 downto 0);
-	signal target_Y_pos: std_logic_vector(9 downto 0); 
-	signal target_X_pos: std_logic_vector(9 downto 0);
-	signal bullet_size: std_logic_vector(9 downto 0); 
-	signal bullet_Y_pos: std_logic_vector(9 downto 0);
-	signal bullet_X_pos: std_logic_vector(9 downto 0);
-	signal tank_on, target_on, bullet_on: std_logic;
+	signal red_data, green_data, blue_data, vert_sync_int, tank_on, target_on, bullet_on: std_logic;
+	signal bullet_fired: std_logic := '0';
+	signal size, bullet_size: std_logic_vector(9 downto 0);
+	signal tank_Y_pos, tank_X_pos: std_logic_vector(9 downto 0);
+	signal target_Y_pos, target_X_pos: std_logic_vector(9 downto 0);
+	signal bullet_Y_pos, bullet_X_pos: std_logic_vector(9 downto 0);
 	signal pixel_row, pixel_column : std_logic_vector(9 downto 0); 
 
 	component vga_sync is
@@ -43,57 +38,73 @@ begin
 		pixel_row => pixel_row, pixel_column => pixel_column);
 				
 				
-tank_size <= CONV_STD_LOGIC_VECTOR(5,10);
-target_size <= CONV_STD_LOGIC_VECTOR(5,10);
+size <= CONV_STD_LOGIC_VECTOR(5,10);
 bullet_size <= CONV_STD_LOGIC_VECTOR(3,10);
 tank_Y_pos <= CONV_STD_LOGIC_VECTOR(100,10);
+tank_X_pos <= CONV_STD_LOGIC_VECTOR(100,10);
 target_Y_pos <= CONV_STD_LOGIC_VECTOR(380,10);
+target_X_pos <= CONV_STD_LOGIC_VECTOR(380,10);
+bullet_Y_pos <= CONV_STD_LOGIC_VECTOR(150,10);
+bullet_X_pos <= CONV_STD_LOGIC_VECTOR(150,10);
 
-vert_sync  <= vert_sync_int;
+vert_sync <= vert_sync_int;
 
-red_data <=  '1' AND NOT target_on AND NOT bullet_on;
-green_data <=  '1' AND NOT tank_on AND NOT bullet_on;
-blue_data <=  '1' AND NOT tank_on AND NOT target_on;
+red_data <=  '1' AND NOT tank_on AND NOT bullet_on;
+green_data <=  '1' AND NOT target_on AND NOT bullet_on;
+blue_data <=  '1' AND NOT target_on AND NOT tank_on;
 
 	-- this process is to display the tank controlled by user
-	create_tank: process(tank_X_pos, tank_Y_pos, tank_size, pixel_column, pixel_row)
+	display_rgb: process(bullet_X_pos, bullet_Y_pos, tank_X_pos, tank_Y_pos, target_X_pos, target_Y_pos, pixel_column, pixel_row, size, bullet_Size)
 	begin
-		if ('0' & tank_X_pos >= pixel_column + tank_size) AND --left boundary
-			(tank_X_pos + tank_size <= '0' & pixel_column) AND --right boundary
-			('0' & tank_Y_pos >= pixel_row + tank_size) AND
-			(tank_Y_pos + tank_size <= '0' & pixel_row)
-		then
-			tank_on <= '1';
-		else
-			tank_on <= '0';
-		end if;
-	end process create_tank;
-	
-	-- this process is to display the targeted tank
-	create_target: process(target_X_pos, target_Y_pos, target_size, pixel_column, pixel_row)
-	begin
-		if ('0' & target_X_pos >= pixel_column + target_size) AND --left boundary
-			(target_X_pos + target_size <= '0' & pixel_column) AND --right boundary
-			('0' & target_Y_pos >= pixel_row + target_size) AND
-			(target_Y_pos + target_size <= '0' & pixel_row)
-		then
+		IF ('0' & target_X_pos <= pixel_column + Size) AND
+		(target_X_pos + Size >= '0' & pixel_column) AND
+		('0' & target_Y_pos <= pixel_row + Size) AND
+		(target_Y_pos + Size >= '0' & pixel_row ) THEN
 			target_on <= '1';
-		else
+		ELSE
 			target_on <= '0';
-		end if;
-	end process create_target;			
-	
-	create_bullet: process(bullet_X_pos, bullet_Y_pos, bullet_size, pixel_column, pixel_row)
-	begin
-		if ('0' & bullet_X_pos >= pixel_column + bullet_size) AND --left boundary
-			(bullet_X_pos + bullet_size <= '0' & pixel_column) AND --right boundary
-			('0' & bullet_Y_pos >= pixel_row + bullet_size) AND
-			(bullet_Y_pos + bullet_size <= '0' & pixel_row)
-		then
+		END IF;
+		
+		IF ('0' & tank_X_pos <= pixel_column + Size) AND
+		(tank_X_pos + Size >= '0' & pixel_column) AND
+		('0' & tank_Y_pos <= pixel_row + Size) AND
+		(tank_Y_pos + Size >= '0' & pixel_row ) THEN
+			tank_on <= '1';
+		ELSE
+			tank_on <= '0';
+		END IF;
+		
+		IF	('0' & Bullet_X_pos <= pixel_column + Bullet_Size) AND
+		(Bullet_X_pos + Bullet_Size >= '0' & pixel_column) AND
+		('0' & Bullet_Y_pos <= pixel_row + Bullet_Size) AND
+		(Bullet_Y_pos + Bullet_Size >= '0' & pixel_row ) THEN
 			bullet_on <= '1';
-		else
+		ELSE
 			bullet_on <= '0';
-		end if;
-	end process create_bullet;
+		END IF;
+	end process display_rgb;
+
+--	movement: process(vert_sync_int)
+--		variable score: integer range 0 to 1001:= 0;
+--		
+--	--move tank
+--			IF (rand >= (CONV_STD_LOGIC_VECTOR(640,11) - Size)) THEN
+--				target_X_pos <= CONV_STD_LOGIC_VECTOR(640,11) - Size;
+--			ELSIF (rand <= Size) THEN
+--				target_X_pos <= Size;
+--			ELSE
+--				target_X_pos <= rand;
+--			END IF;
+--			
+--			IF (rand(5) = '1') THEN
+--				Ball_X_motion <= - CONV_STD_LOGIC_VECTOR(2,11);
+--			ELSE	
+--				Ball_X_motion <= CONV_STD_LOGIC_VECTOR(2,11);
+--			END IF;
+--			s_active <= '0';
+--			Player_X_pos <= CONV_STD_LOGIC_VECTOR(320, 11);
+	--move target
+	--control when bullet is fired
+			
 	
 end architecture;
